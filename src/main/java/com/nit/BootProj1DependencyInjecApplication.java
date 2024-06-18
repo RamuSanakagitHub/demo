469a0b311f87dfc73ff42fb362bf609d15b4268e
Jcoder224 <gulshan9305504804@gmail.com>
2024-06-18 14:31:28 +0530

    First commit

---
+++ src/main/java/com/nit/BootProj1DependencyInjecApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj1DependencyInjecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj1DependencyInjecApplication.class, args);
+	}
+
+}
